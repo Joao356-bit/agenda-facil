fbf3b3121f3918aab3bc4f01684e98d90c847e5c
Joao356-bit <jonasluis3401@gmail.com>
2026-05-18 09:31:08 -0300

    estrutura dashboard criada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login';
+import { CadastroComponent } from './pages/cadastro/cadastro';
+import { DashboardComponent } from './pages/dashboard/dashboard';
+import { ProfissionaisComponent } from './pages/profissionais/profissionais';
+import { AgendaComponent } from './pages/agenda/agenda';
+import { PerfilComponent } from './pages/perfil/perfil';
+import { NovoAgendamentoComponent } from './pages/novo-agendamento/novo-agendamento';
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profissionais', component: ProfissionaisComponent },
+  { path: 'agenda', component: AgendaComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'novo-agendamento', component: NovoAgendamentoComponent },
+];
---
+++ src/app/pages/novo-agendamento/novo-agendamento.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-agendamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './novo-agendamento.html',
+  styleUrl: './novo-agendamento.css'
+})
+export class NovoAgendamentoComponent {
+
+}
